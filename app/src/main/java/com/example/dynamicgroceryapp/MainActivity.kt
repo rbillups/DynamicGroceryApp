06acc1f9eb13edc0f48f58eb632ef8b3a2ea1020
Koby <kobysysouvanh@gmail.com>
2023-02-22 16:30:13 -0500

    added firebase cloud database to application

--- app/src/main/java/com/example/dynamicgroceryapp/MainActivity.kt
+++ app/src/main/java/com/example/dynamicgroceryapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.dynamicgroceryapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
