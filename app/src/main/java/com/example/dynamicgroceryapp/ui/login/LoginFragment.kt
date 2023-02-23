e068fe69f13332d3546b4dd8b2fdadd5e4a447ca
Koby <kobysysouvanh@gmail.com>
2023-02-23 11:24:49 -0500

    added login feature and fixed some issues with textField

--- app/src/main/java/com/example/dynamicgroceryapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/dynamicgroceryapp/ui/login/LoginFragment.kt
@@ -1,12 +1,16 @@
 package com.example.dynamicgroceryapp.ui.login
 
+import android.content.ContentValues.TAG
+import android.content.Context
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextUtils
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,19 +24,26 @@ import androidx.navigation.findNavController
 import com.example.dynamicgroceryapp.databinding.FragmentLoginBinding
 
 import com.example.dynamicgroceryapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.net.PasswordAuthentication
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+        auth = FirebaseAuth.getInstance()
+
+        val email = binding.loginEmail
+        val password = binding.loginPassword
 
         binding.createAccountButton.setOnClickListener{
                 view: View->
@@ -46,6 +57,33 @@ class LoginFragment : Fragment() {
                 .navigate(R.id.action_loginFragment_to_accountRecoveryFragment)
         }
 
+        binding.signInButton.setOnClickListener{
+            val email: String = email.text.toString()
+            val password: String = password.text.toString()
+            var stop: Boolean = true
+
+            if (email.isEmpty() || password.isEmpty()){
+                showToastMessage(requireContext(), "Invalid Email/Password")
+            }
+            else {
+                stop = false
+            }
+
+            if (!stop) {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            showToastMessage(requireContext(), "Signed in Successfully")
+
+                        } else {
+                            showToastMessage(requireContext(), "Invalid Email/Password")
+                        }
+                    }
+            }
+
+
+        }
+
 
         return binding.root
 
@@ -55,6 +93,13 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
+    }
+
+    //Create toast method
+    fun showToastMessage(context: Context, message: String) {
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(context, message, duration)
+        toast.show()
     }
 
 
