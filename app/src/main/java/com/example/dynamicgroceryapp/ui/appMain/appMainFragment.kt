26ddfc532f3e8d055fa98542ebe50d4cdef43c40
katzhop <kaitnich22@gmail.com>
2023-03-14 13:20:49 -0400

    Cleaning up a couple of screen layouts to fit better

--- app/src/main/java/com/example/dynamicgroceryapp/ui/appMain/appMainFragment.kt
+++ app/src/main/java/com/example/dynamicgroceryapp/ui/appMain/appMainFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.dynamicgroceryapp.R
 import com.example.dynamicgroceryapp.databinding.FragmentAppMainBinding
