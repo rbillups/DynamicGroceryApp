680552f2429cc7553c045b0467c7e12a726b85f7
rbillups <reginaldbillups45@gmail.com>
2023-04-27 01:47:34 -0400

    -Commented out the list fragment(no functionality)

--- app/src/main/java/com/example/dynamicgroceryapp/ui/appMain/appMainFragment.kt
+++ app/src/main/java/com/example/dynamicgroceryapp/ui/appMain/appMainFragment.kt
@@ -44,7 +44,7 @@ class appMainFragment : Fragment() {
         //bottom nav on click
         bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-
+               // R.id.listBottomNav -> viewPager.currentItem = 0
                 R.id.groupBottomNav -> viewPager.currentItem = 0
                 R.id.friendsBottomNav -> viewPager.currentItem = 1
                 R.id.profileBottomNav -> viewPager.currentItem = 2
